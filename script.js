404227a77b1b697fe9d8733ac7c98b73b1573782
ul4sfak1r <ulasfakir66@gmail.com>
2024-10-25 09:27:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
-const ReadMoreButton = document.getElementById("ReadMoreButton");
+const menu = document.getElementById("menu-icon");
+const body = document.body;
+const header = document.getElementById("header");
 
-ReadMoreButton.addEventListener("click", () => {
-	window.open("https://www.google.com", "_blank");
+menu.addEventListener("click", () => {
+    
+    if (!document.getElementById("menuButton")) {
+        header.insertAdjacentHTML("beforeend", `
+            <div class="menu-button" id="menuButton">
+                <div class="menu-wrapper">
+                    <div class="icon-menu-close-div">
+                        <img src="icon-menu-close.svg" class="icon-menu-close" id="closeMenu">
+                    </div>
+                    <nav class="mobile-nav">
+                        <ul>
+                            <li>Home</li>
+                            <li>New</li>
+                            <li>Popular</li>
+                            <li>Trending</li>
+                            <li>Categories</li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        `);
+    }
+
+    if (!document.getElementById("overlay")) {
+        body.insertAdjacentHTML("beforeend", `
+            <div class="overlay" id="overlay"></div>
+        `);
+    }
+
+    document.getElementById("closeMenu").addEventListener("click", closeMenu);
+    document.getElementById("overlay").addEventListener("click", closeMenu);
 });
+
+
+function closeMenu() {
+    const menuButton = document.getElementById("menuButton"); 
+    const overlay = document.getElementById("overlay");
+    
+    if (menuButton) {
+        menuButton.remove(); 
+    }
+    if (overlay) {
+        overlay.remove(); 
+    }
+}

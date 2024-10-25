ee1b5f9a571b2ccd002bc4acb9ac430d8943a336
ul4sfak1r <ulasfakir66@gmail.com>
2024-10-25 12:22:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,38 +2,45 @@ const menu = document.getElementById("menu-icon");
 const body = document.body;
 const header = document.getElementById("header");
 
+
 menu.addEventListener("click", () => {
     
     if (!document.getElementById("menuButton")) {
-        header.insertAdjacentHTML("beforeend", `
-            <div class="menu-button" id="menuButton">
-                <div class="menu-wrapper">
-                    <div class="icon-menu-close-div">
-                        <img src="icon-menu-close.svg" class="icon-menu-close" id="closeMenu">
-                    </div>
-                    <nav class="mobile-nav">
-                        <ul>
-                            <li>Home</li>
-                            <li>New</li>
-                            <li>Popular</li>
-                            <li>Trending</li>
-                            <li>Categories</li>
-                        </ul>
-                    </nav>
+        openMenu();
+    } else {
+        closeMenu(); 
+    }
+});
+
+
+function openMenu() {
+    header.insertAdjacentHTML("beforeend", `
+        <div class="menu-button" id="menuButton">
+            <div class="menu-wrapper">
+                <div class="icon-menu-close-div">
+                    <img src="icon-menu-close.svg" class="icon-menu-close" id="closeMenu">
                 </div>
+                <nav class="mobile-nav">
+                    <ul>
+                        <li>Home</li>
+                        <li>New</li>
+                        <li>Popular</li>
+                        <li>Trending</li>
+                        <li>Categories</li>
+                    </ul>
+                </nav>
             </div>
-        `);
-    }
+        </div>
+    `);
 
-    if (!document.getElementById("overlay")) {
-        body.insertAdjacentHTML("beforeend", `
-            <div class="overlay" id="overlay"></div>
-        `);
-    }
+    body.insertAdjacentHTML("beforeend", `
+        <div class="overlay" id="overlay"></div>
+    `);
 
+   
     document.getElementById("closeMenu").addEventListener("click", closeMenu);
     document.getElementById("overlay").addEventListener("click", closeMenu);
-});
+}
 
 
 function closeMenu() {
